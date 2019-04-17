acb07f4d7585d55a29199c013d71721caaf34989
Agnieszka Lempaszek <agnieszka.lempaszek@student.uj.edu.pl>
2019-04-17 13:41:21 +0200

    Fixing input validation

---
+++ gulpfile.js
@@ -0,0 +1,25 @@
+var gulp = require('gulp');
+var browserSync = require('browser-sync').create();
+
+gulp.task("Hello", (done) => {
+    console.log("Hello");
+    done();
+});
+
+gulp.task('browserSync', function (done) {
+    browserSync.init({
+        server: {
+            baseDir: 'app'
+        },
+    });
+    done();
+});
+
+gulp.task('watch', function(done){
+    gulp.watch('app/js/**/*.js', browserSync.reload);
+    done();
+});
+
+gulp.task('default', gulp.series('browserSync', 'watch', function (done) {
+    done();
+}));
